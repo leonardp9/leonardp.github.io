f59e81e805caf9955f0f24695c581022933860ad
leonard <leonard.popescu@indsoft.ro>
2023-04-03 15:46:40 +0300

    modificari pentru evenimente pe fiecare buton din meniu

--- index.js
+++ index.js
@@ -7,4 +7,30 @@ function showSkills() {
   console.warn("skills");
   document.getElementById("home").style.display = "none";
   document.getElementById("skills").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showProjects() {
+  console.warn("skills");
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showLanguages() {
+  console.warn("skills");
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "block";
+}
+
+function showHome() {
+  console.warn("skills");
+  document.getElementById("home").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
 }
